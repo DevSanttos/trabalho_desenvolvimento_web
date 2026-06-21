218fe7aa5e314d906bb1c542c859542acd10830e
Nataniel Oliveira da Silva <onataniel@ymail.com>
2026-06-21 16:34:31 -0300

    Backend

---
+++ Vitrinelocal/src/main/java/br/com/vitrinelocal/VitrinelocalApplication.java
@@ -0,0 +1,13 @@
+package br.com.vitrinelocal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitrinelocalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VitrinelocalApplication.class, args);
+    }
+
+}
---
+++ Vitrinelocal/src/test/java/br/com/vitrinelocal/VitrinelocalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vitrinelocal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitrinelocalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
